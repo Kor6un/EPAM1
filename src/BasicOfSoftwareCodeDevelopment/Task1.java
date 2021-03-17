8e684b19c9ff6aaaa42e0f0cc68f7404d0ac7858
kor6un <Zmey_85@bk.ru>
2021-03-17 19:28:16 +0300

    Task1 added

---
+++ src/BasicOfSoftwareCodeDevelopment/Task1.java
@@ -0,0 +1,29 @@
+package BasicOfSoftwareCodeDevelopment;
+
+/*
+
+ Найдите значение функции: z = ((a - 3) * b / 2 + c
+
+*/
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        double a, b, c;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input a: ");
+        a = scanner.nextDouble();
+
+        System.out.print("Input b: ");
+        b = scanner.nextDouble();
+
+        System.out.print("Input c: ");
+        c = scanner.nextDouble();
+
+        System.out.println("z = " + ((a - 3) * b / 2 + c));
+    }
+}
