24bcaef496a7cf5cf60573b54d56a2fe0798db06
kor6un <Zmey_85@bk.ru>
2021-03-21 17:01:25 +0300

    Cycles2 class added

---
+++ src/BasicOfSoftwareCodeDevelopment/Cycles2.java
@@ -0,0 +1,34 @@
+package BasicOfSoftwareCodeDevelopment;
+
+import java.util.Scanner;
+
+public class Cycles2 {
+    public static void main(String[] args) {
+
+        float a, b, h;
+
+        System.out.println("Enter segment coordinates (a, b): ");
+
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextFloat();
+        b = scanner.nextFloat();
+
+        System.out.println("Enter increment h: ");
+        h = scanner.nextFloat();
+        scanner.close();
+
+       do {
+           if (a > b) {
+               System.out.println("Incorrect input! You must enter a < b!");
+               break;
+           } else {
+               if (a > 2) {
+                   System.out.println(a);
+               } else {
+                   System.out.println(-a);
+               }
+               a = a + h;
+           }
+       } while (a <= b);
+    }
+}
