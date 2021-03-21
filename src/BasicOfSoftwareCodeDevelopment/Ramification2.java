fd8f813ba9dbc827486094aacd62a2fb4a9feb9d
kor6un <Zmey_85@bk.ru>
2021-03-21 14:31:45 +0300

    Ramification2 class added

---
+++ src/BasicOfSoftwareCodeDevelopment/Ramification2.java
@@ -0,0 +1,25 @@
+package BasicOfSoftwareCodeDevelopment;
+
+/*
+
+Найти max{min(a,b), min(c,d)}
+
+*/
+
+import java.util.Scanner;
+
+public class Ramification2 {
+    public static void main(String[] args) {
+        System.out.println("Enter a, b, c, d:");
+
+        double a, b, c, d;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        c = scanner.nextDouble();
+        d = scanner.nextDouble();
+        scanner.close();
+
+        System.out.println(Math.max(Math.min(a, b), Math.min(c, d)));
+    }
+}
