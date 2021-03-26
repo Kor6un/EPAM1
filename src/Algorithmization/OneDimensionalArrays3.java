84e80faf09130d5e1d989099691959184785bde1
kor6un <Zmey_85@bk.ru>
2021-03-27 00:05:19 +0300

    OneDimensionalArrays3 added

---
+++ src/Algorithmization/OneDimensionalArrays3.java
@@ -0,0 +1,39 @@
+package Algorithmization;
+
+import java.util.Scanner;
+
+public class OneDimensionalArrays3 {
+    public static void main(String[] args) {
+
+        int n;
+        int negative = 0;
+        int zero = 0;
+
+        System.out.println("Enter the number of array size n: ");
+
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        scanner.close();
+
+        int[] someArray = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            someArray[i] = (int) (Math.random()*20)-10;
+
+            if (someArray[i] < 0) {
+                negative += 1;
+            } else {
+                if (someArray[i] == 0) {
+                    zero += 1;
+                }
+            }
+            System.out.println(someArray[i]);
+        }
+
+        System.out.println("Positive: " + (n - negative - zero) + "\n" +
+                "negative: " + negative + "\n" +
+                "zero :" + zero);
+
+    }
+}
