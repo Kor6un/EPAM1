1d897e85e6f71b8b32cd2669b8cb454724b195ff
kor6un <Zmey_85@bk.ru>
2021-03-30 22:25:20 +0300

    find simple number algorithm get from wiki

--- src/Algorithmization/OneDimensionalArrays6.java
+++ src/Algorithmization/OneDimensionalArrays6.java
@@ -28,13 +28,28 @@ public class OneDimensionalArrays6 {
         //TODO
         for (int i = 0; i < n; i++) {
             doubles[i] = Double.parseDouble(decimalFormat.format(Math.random() * 15));
-            System.out.println(i + ": " + doubles[i] + ";");
-            for (int j = 1; j <= n; j++) {
-                if (i+1 % j == 0 && i+1 == j) {
-                    sum += doubles[i];
-                }
+            System.out.println((i + 1) + ": " + doubles[i] + "; ");
+            if (checkSimple(i + 1)) {
+                sum += doubles[i];
+                System.out.println ("Simple (" + (i + 1) + ") : " + doubles[i] + " ");
             }
         }
         System.out.println("Sum: " + decimalFormat.format(sum));
+    }
+
+    public static boolean checkSimple(int i){
+        if (i<=1)
+            return false;
+        else if (i <=3)
+            return true;
+        else if (i%2==0 || i %3 ==0)
+            return false;
+        int n = 5;
+        while (n*n <=i){
+            if (i % n ==0 || i % (n+2) == 0)
+                return false;
+            n=n+6;
+        }
+        return true;
     }
 }
