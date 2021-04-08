a22a55cf6fd57794875c26619c86e58a8033e610
kor6un <Zmey_85@bk.ru>
2021-04-08 20:32:13 +0300

    TwoDimensionalArrays1 added

---
+++ src/Algorithmization/TwoDimensionalArrays1.java
@@ -0,0 +1,50 @@
+package Algorithmization;
+
+import java.util.Scanner;
+
+public class TwoDimensionalArrays1 {
+    public static void main(String[] args) {
+
+        int n;
+
+        System.out.print("Enter the size of array n: ");
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        scanner.close();
+
+        int[][] ints = getRandomMatrix(n);
+
+        System.out.println("Original matrix: ");
+        display(ints);
+
+        System.out.println("Changed matrix: ");
+        for (int i = 0; i < ints.length; i++) {
+            for (int j = 0; j < ints.length; j++) {
+                if ((j+1) % 2 != 0) {
+                    System.out.print(ints[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static void display(int[][] ints) {
+        for (int i = 0; i < ints.length; i++) {
+            for (int j = 0; j < ints.length; j++) {
+                System.out.print(ints[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] getRandomMatrix(int size) {
+        int[][] ints = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                ints[i][j] = (int) (Math.random()* 10);
+            }
+        }
+        return ints;
+    }
+}
