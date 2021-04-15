a6888956e27afb75e997440f1ceca958c6b0060c
Kor6un <Zmey_85@bk.ru>
2021-04-16 00:11:51 +0300

    TwoDimensionalArrays14 is work

--- src/Algorithmization/TwoDimensionalArrays14.java
+++ src/Algorithmization/TwoDimensionalArrays14.java
@@ -7,17 +7,25 @@ public class TwoDimensionalArrays14 {
     public static void main(String[] args) {
         int n,m;
 
-        System.out.print("Enter count rows n: ");
-        n = getInt();
+        do {
+            System.out.print("Enter count rows n: ");
+            n = getInt();
 
-        System.out.print("Enter count columns m: ");
-        m = getInt();
+            System.out.print("Enter count columns m: ");
+            m = getInt();
+
+            System.out.println("Count columns must be less the count rows!\nPlease, try again!");
+        } while (n < m);
 
         int[][] matrix = getMatrix(n, m);
 
         System.out.println("\nOriginal array: ");
         display(matrix);
 
+        System.out.println("Counts: ");
+        for (int i = 0; i < matrix[0].length; i++) {
+            System.out.print(getCount(matrix, i) + " ");
+        }
     }
 
     private static void display(int[][] ints) {
@@ -33,26 +41,28 @@ public class TwoDimensionalArrays14 {
 
     private static int[][] getMatrix(int line, int column) {
         int[][] ints = new int[line][column];
-//TODO
         for (int i = 0; i < column; i++) {
             for (int j = 0; j < line; j++) {
                 ints[j][i] = (int) (Math.random() * 2);
             }
+            if (getCount(ints, i) != i+1) {
+                i--;
+            }
         }
-
         return ints;
     }
 
     private static int getCount(int[][] ints, int index) {
         int count = 0;
         for (int i = 0; i < ints.length; i++) {
-            count += 1;
+            if (ints[i][index] != 0) {
+                count += 1;
+            }
         }
         return count;
     }
 
     private static int getInt(){
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        return new Scanner(System.in).nextInt();
     }
 }
