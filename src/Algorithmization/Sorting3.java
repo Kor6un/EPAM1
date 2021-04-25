3d2fc90a02e2e76e8ffc71cd4811c06bfe6dab39
Kor6un <Zmey_85@bk.ru>
2021-04-25 21:43:50 +0300

    selectionSort() method added

--- src/Algorithmization/Sorting3.java
+++ src/Algorithmization/Sorting3.java
@@ -1,12 +1,47 @@
 package Algorithmization;
 
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 //сортировка выбором
+
 public class Sorting3 {
     public static void main(String[] args) {
+        int size;
 
+        System.out.print("Enter the size of array: ");
+        size = getInt();
+
+        int[] ints = getRandomIntArray(size);
+        Arrays.sort(ints);
+
+        System.out.println("Original array: " + Arrays.toString(ints));
+
+        long start = System.nanoTime();
+        selectionSort(ints);
+        long end = System.nanoTime();
+
+        System.out.println("Changed  array: " + Arrays.toString(ints) +"\nTime: " + (end-start));
+    }
+
+    private static int[] selectionSort(int[] ints) {
+        int size = ints.length;
+        int temp;
+        for (int i = 0; i < size; i++) {
+            int index = i;
+            int max = ints[i];
+            for (int j = i; j < size; j++) {
+                if (ints[j] > max) {
+                    max = ints[j];
+                    index = j;
+                }
+            }
+            temp = ints[i];
+            ints[i] = max;
+            ints[index] = temp;
+            size--;
+        }
+        return ints;
     }
 
     private static int[] getRandomIntArray(int size) {
