20ce2a115199894d16bcd2836ec855d9e6f9f688
Kor6un <Zmey_85@bk.ru>
2021-05-08 22:53:06 +0300

    CharArray3 refactor

--- src/StringAndBasicOfTextProcessing/StringToCharArray/CharArray3.java
+++ src/StringAndBasicOfTextProcessing/StringToCharArray/CharArray3.java
@@ -8,20 +8,15 @@ public class CharArray3 {
         String input3 = "1556";
         String input4 = "sds1";
 
-        char[] chars = input.toCharArray();
-        char[] chars1 = input1.toCharArray();
-        char[] chars2 = input2.toCharArray();
-        char[] chars3 = input3.toCharArray();
-        char[] chars4 = input4.toCharArray();
-
-        System.out.println("Count digits: " + getCountDigits(chars));
-        System.out.println("Count digits1: " + getCountDigits(chars1));
-        System.out.println("Count digits2: " + getCountDigits(chars2));
-        System.out.println("Count digits3: " + getCountDigits(chars3));
-        System.out.println("Count digits4: " + getCountDigits(chars4));
+        System.out.println("Count digits: " + getCountDigits(input));
+        System.out.println("Count digits1: " + getCountDigits(input1));
+        System.out.println("Count digits2: " + getCountDigits(input2));
+        System.out.println("Count digits3: " + getCountDigits(input3));
+        System.out.println("Count digits4: " + getCountDigits(input4));
     }
 
-    private static int getCountDigits(char[] chars) {
+    private static int getCountDigits(String s) {
+        char[] chars = s.toCharArray();
         int count = 0;
         for (char c : chars) {
             if (c >= 48 && c <= 57) {
